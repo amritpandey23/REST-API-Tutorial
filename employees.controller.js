e3b99425b7b53451673c7016649087f74776d85e
ap4gh <amritpandey@tuta.io>
2019-08-24 18:52:33 +0530

    update

--- employees.controller.js
+++ employees.controller.js
@@ -1,48 +1,37 @@
-const data = require("./data.json");
-
 function list_all_employees(req, res) {
-    // let { db_connection } = req.app.locals;
-    // db_connection.query(`
-    //     SELECT e.id, e.name, e.address, e.salary,
-    //     d.name as department
-    //     FROM employees e
-    //     INNER JOIN departments d
-    //     ON e.department = d.id
-    //     WHERE e.salary > 150000;
-    // `, function(err, results) {
-    //     if (err) {
-    //         return res.status(500).json(err);
-    //     }
-    //     return res.status(200).json(results);
-    // });
+  let { knex } = req.app.locals;
 
-    let { knex } = req.app.locals;
-    knex
-        .select("name", "address", "salary")
-        .from("employees")
-            .then(function(data) {
-                res.status(200).json(data);
-            })
-            .catch(function(err) {
-                console.error("ERROR:", err);
-            });
+  knex
+    .select("name", "address", "salary")
+    .from("employees")
+    .then(function (data) {
+      res.status(200).json(data);
+    })
+    .catch(function (err) {
+      console.error("ERROR:", err);
+    });
+  
+  return console.log(`GET: list all employees.`);
 }
 
 function list_single_employee(req, res) {
-    let { knex } = req.app.locals;
-    let { id } = req.params;
-    knex
-        .select("name", "address", "salary")
-        .from("employees")
-        .where({ id: `${id}` })
-            .then(function(data) {
-                return data.length ? 
-                res.status(200).json(data) :
-                res.status(404).end("Employee not found.");
-            })
-            .catch(function(err) {
-                console.error("ERROR:", err);
-            });
+  let { knex } = req.app.locals;
+  let { id } = req.params;
+  // PROC: ...
+  knex
+    .select("name", "address", "salary")
+    .from("employees")
+    .where({ id: `${id}` })
+    .then(function (data) {
+      return data.length ?
+        res.status(200).json(data) :
+        res.status(404).end("Employee not found.");
+    })
+    .catch(function (err) {
+      console.error("ERROR:", err);
+    });
+  
+  return console.log(`GET: list single employee id: ${id}`);
 }
 
 module.exports = { list_all_employees, list_single_employee };
