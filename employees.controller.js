f8c888ef2d753a3c00c808be699cd89429e1ff29
ap4gh <amritpandey@tuta.io>
2019-08-24 17:19:31 +0530

    video 24: using mysql driver

--- employees.controller.js
+++ employees.controller.js
@@ -1,11 +1,22 @@
 const data = require("./data.json");
 
-function listAllEmployees(req, res) {
+function list_all_employees(req, res) {
     // res.statusCode = 200;
     // return res.json(data);
-    return res.status(200).json(data);
+    let { db_connection } = req.app.locals;
+    db_connection.query(`
+        SELECT e.id, e.name, e.address, e.salary,
+        d.name as department
+        FROM employees e
+        INNER JOIN departments d
+        ON e.department = d.id
+        WHERE e.salary > 150000;
+    `, function(err, results) {
+        if (err) {
+            return res.status(500).json(err);
+        }
+        return res.status(200).json(results);
+    });
 }
 
-module.exports = {
-    listAllEmployees
-};
+module.exports = { list_all_employees };
