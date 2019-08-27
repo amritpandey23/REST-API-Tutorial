8236d30271459a30133def045ac508eba6af74bc
ap4gh <amrit.github@tuta.io>
2019-08-27 19:05:42 +0530

    update

--- status_messages.js
+++ status_messages.js
@@ -4,13 +4,22 @@ const
 module.exports = {
 
     // employee
+    EMPLOYEE_CREATED: `employee created successfully`,
     EMPLOYEE_NOT_FOUND: function(id) { return `employee id: ${id}, not found` },
     EMPLOYEE_DELETED: function(id) { return `employee id: ${id}, was deleted` },
     EMPLOYEE_UPDATED: function(id) { return `employee id: ${id}, was updated` },
-    EMPLOYEE_CREATED: `employee created successfully`,
+
+    // department
+    DEPARTMENT_CREATED: `department created successfully`,
+    NAME_REQ: `you need to add a name`,
+    DEPARTMENT_NOT_FOUND: function(id) { return `department id: ${id}, not found` },
+    DEPARTMENT_DELETED: function(id) { return `department id: ${id}, was deleted` },
+    DEPARTMENT_UPDATED: function(id) { return `department id: ${id}, was updated` },
 
     // database
     COLUMNS_REQ: function(columns) { return `field values required: ${columns.join(' ')}` },
+    DATABASE_ERROR: `error in database`,
+    SQL_ERROR: function(err) { return colors.red(`SQL ERROR: ${err.sqlMessage}`) },
 
     // HTTP methods
     GET: function(desc) { return colors.cyan(`GET: ${desc}`) },
